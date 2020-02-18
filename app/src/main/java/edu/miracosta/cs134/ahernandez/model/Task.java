19facd1c059389b80186ad27e9e5cee59be8e859
4804W19 <4804W19@usa.miracosta.cc.ca.us>
2020-02-18 12:49:58 -0800

    Initial commit

---
+++ app/src/main/java/edu/miracosta/cs134/ahernandez/model/Task.java
@@ -0,0 +1,57 @@
+package edu.miracosta.cs134.ahernandez.model;
+
+public class Task {
+
+    private int mId ;
+    private String mDescription ;
+    private boolean mIsDone ;
+
+    public Task(int id, String description, boolean isDone) {
+        mId = id;
+        mDescription = description;
+        mIsDone = isDone;
+    }
+
+    public Task (String description, boolean isDone)
+    {
+        this(-1, description, isDone) ;
+    }
+
+    public Task(String description)
+    {
+        this(-1,description, false) ;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public boolean isDone() {
+        return mIsDone;
+    }
+
+    public void setDone(boolean isDone) {
+        mIsDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + mId +
+                ", description='" + mDescription + '\'' +
+                ", isDone=" + mIsDone +
+                '}';
+    }
+}
